3fdd4b042983291c7c03462c625d9423d12a030a
Pedro Stein <65890570+Pedro6Stein@users.noreply.github.com>
2026-03-01 13:49:57 -0300

    feat(entities): implement user and task domain contracts (DTOs)

---
+++ frontend/entities/task/model/types.ts
@@ -0,0 +1,25 @@
+﻿export enum TaskStatus {
+    PENDING = 1,
+    COMPLETED = 2
+}
+
+export interface Task {
+    id: string;
+    managerId: string;
+    assigneeId: string;
+    description: string;
+    dueDate: string;
+    status: TaskStatus;
+    createdAt: string;
+    completedAt?: string | null;
+}
+
+export interface TaskWithAssigneeDTO extends Omit<Task, 'managerId' | 'assigneeId'> {
+    assignee: {
+        fullName: string;
+        email?: string;
+    };
+    manager?: {
+        fullName: string;
+    };
+}
---
+++ frontend/entities/user/model/types.ts
@@ -0,0 +1,19 @@
+﻿export interface User {
+    id: string;
+    fullName: string;
+    birthDate: string;
+    landline?: string | null;
+    mobile: string;
+    email: string;
+    address: string;
+    photo?: string | null;
+    isManager: boolean;
+    createdAt: string;
+}
+
+export interface AuthUserDTO {
+    id: string;
+    fullName: string;
+    email: string;
+    isManager: boolean;
+}
